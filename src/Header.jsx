3848dacfc6af88f9978e305927cc5e447db965f9
saurabh <vksaha99340@gmail.com>
2022-10-01 18:25:35 +0530

    Initial Setup & partial header component work done

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt="linkedin__icon"
+        />
+        <div className="header__search">
+          {/* searchicon */}
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
