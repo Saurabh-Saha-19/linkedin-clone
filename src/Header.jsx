e807c15d4f589760f2f9940ec13a73cb55ed8f40
saurabh <vksaha99340@gmail.com>
2022-10-06 23:14:03 +0530

    Header Completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,9 @@ import SearchIcon from "@mui/icons-material/Search";
 import HeaderOption from "./HeaderOption";
 import HomeIcon from "@mui/icons-material/Home";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
 
 function Header() {
   return (
@@ -23,6 +26,10 @@ function Header() {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C5603AQFw-Hf2ydH9QQ/profile-displayphoto-shrink_800_800/0/1659755310583?e=1670457600&v=beta&t=-Oh_6TlQbDOJtljHn93j6poVIOgTEkT5reG3EG6OczA" title="Me" />
       </div>
     </div>
   );
